9077d99aac00e34b51e7cfe1ef98d997f9ccd0c0
1NFERNO-HUB <244408626+1NFERNO-HUB@users.noreply.github.com>
2025-12-17 11:59:09 -0600

    Update cli.py

--- bridge/cli.py
+++ bridge/cli.py
@@ -1,1 +1,14 @@
+import argparse
+from core.pipeline import run_obfuscation
 
+def start_cli():
+    print("--- 1NFERNO OBFUSCATOR ---")
+    parser = argparse.ArgumentParser(description="Professional Lua Obfuscator")
+    parser.add_argument("-i", "--input", help="The Lua file to protect", required=True)
+    parser.add_argument("-o", "--output", help="Where to save the result", default="out.lua")
+    
+    args = parser.parse_args()
+    
+    # Send the info to the engine
+    run_obfuscation(args.input, args.output)
+    print(f"Successfully protected {args.input}!")
