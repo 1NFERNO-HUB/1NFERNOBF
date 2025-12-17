264bad618b5fe651eff3e9d30d2b056256c50b96
1NFERNO-HUB <244408626+1NFERNO-HUB@users.noreply.github.com>
2025-12-17 12:28:30 -0600

    Update __init__.py

--- transformer/__init__.py
+++ transformer/__init__.py
@@ -1,1 +1,5 @@
-
+# transformer/__init__.py
+from .constant_escaper import ConstantEscaper
+from .control_flow import ControlFlowFlattener
+from .junk_code import JunkInjector
+from .opaque_predicates import OpaquePredicateGenerator
