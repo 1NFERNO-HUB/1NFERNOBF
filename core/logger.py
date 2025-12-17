3a83c9ea876b078552f00e5c7021bb110ebb9a7a
1NFERNO-HUB <244408626+1NFERNO-HUB@users.noreply.github.com>
2025-12-17 12:15:19 -0600

    Update logger.py

--- core/logger.py
+++ core/logger.py
@@ -1,1 +1,21 @@
+# core/logger.py
+import os
+from termcolor import colored # pip install termcolor
 
+class Logger:
+    @staticmethod
+    def info(message):
+        print(colored(f"[*] {message}", "blue"))
+
+    @staticmethod
+    def success(message):
+        print(colored(f"[+] {message}", "green", attrs=['bold']))
+
+    @staticmethod
+    def error(message):
+        print(colored(f"[!] ERROR: {message}", "red", attrs=['bold']))
+
+    @staticmethod
+    def banner():
+        # Your cool ASCII art for 1NFERNO goes here
+        print(colored("1NFERNO OBFUSCATOR v1.0", "red", attrs=['bold', 'blink']))
