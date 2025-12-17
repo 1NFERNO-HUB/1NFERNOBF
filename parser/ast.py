626202b7978f5feb3901e5f14e45a2c6fb3b0b68
1NFERNO-HUB <244408626+1NFERNO-HUB@users.noreply.github.com>
2025-12-17 12:25:13 -0600

    Update ast.py

--- parser/ast.py
+++ parser/ast.py
@@ -1,1 +1,21 @@
+class Node:
+    """Base class for all AST nodes."""
+    pass
 
+class Statement(Node): pass
+class Expression(Node): pass
+
+class Block(Node):
+    def __init__(self, body):
+        self.body = body # A list of Statements
+
+class LocalAssign(Statement):
+    def __init__(self, targets, values):
+        self.targets = targets # Names like 'x', 'y'
+        self.values = values   # Values like 10, 20
+
+class BinaryOp(Expression):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op # +, -, *, etc.
+        self.right = right
